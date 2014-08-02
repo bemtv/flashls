8896c295ee1ce4df680f32db7bde2c9d496adc48
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-08-02 15:42:24 -0300

    update build

--- build/build_bemtv.sh
+++ build/build_bemtv.sh
@@ -20,41 +20,13 @@ OPT_RELEASE="-library-path+=../lib/as3crypto.swc \
 echo "Compiling bin/debug/P2PHLSPlayer.swf"
 $FLEXPATH/bin/mxmlc ../src/tv/bem/BemTVPlayer.as \
     -source-path ../src \
-    -o /Users/flavio.barbosa/Development/masters/clappr-p2phls-plugin/public/P2PHLSPlayer.swf \
+    -o /Users/flavio.barbosa/Development/bemtv/clappr-p2phls-plugin/public/P2PHLSPlayer.swf \
     $OPT_RELEASE \
     -library-path+=../lib/blooddy_crypto.swc \
     -target-player="11.5" \
     -default-size 480 270 \
     -default-background-color=0x000000 \
     -default-frame-rate=60
-./add-opt-in.py /Users/flavio.barbosa/Development/masters/clappr-p2phls-plugin/public/P2PHLSPlayer.swf
+./add-opt-in.py /Users/flavio.barbosa/Development/bemtv/clappr-p2phls-plugin/public/P2PHLSPlayer.swf
 
-#echo "Compiling bin/debug/flashls.swc"
-#$FLEXPATH/bin/compc \
-#    $OPT_DEBUG \
-#    -include-sources ../src/com \
-#    -include-sources ../src/org \
-#    -include-sources ../src/tv \
-#    -output ../bin/debug/flashls.swc \
-#    -target-player="11.5"
-#
-#echo "Compiling bin/release/flashls.swc"
-#$FLEXPATH/bin/compc \
-#    $OPT_RELEASE \
-#    -include-sources ../src/org/mangui/hls \
-#    -output ../bin/release/flashls.swc \
-#    -target-player="11.5"
-
-#echo "Compiling bin/release/P2PHLSPlayer.swf"
-#$FLEXPATH/bin/mxmlc ../src/org/mangui/chromeless/ChromelessPlayer.as \
-#    -source-path ../src \
-#    -o ../bin/release/HLSPlayer.swf \
-#    $OPT_RELEASE \
-#    -library-path+=../lib/blooddy_crypto.swc \
-#    -target-player="11.5" \
-#    -default-size 480 270 \
-#    -default-background-color=0x000000 \
-#    -default-frame-rate=60
-#./add-opt-in.py ../bin/release/P2PHLSPlayer.swf
-#
 
